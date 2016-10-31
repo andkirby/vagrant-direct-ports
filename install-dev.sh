dfd27d868a87bf41015d0f16a2a023de9f64362a
Andrew Roslik <andkirby@gmail.com>
2016-11-01 00:03:06 +0200

    Added smart checking bundler version.

--- install-dev.sh
+++ install-dev.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Wake up project
+# bash install-dev.sh [clean]
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -10,6 +13,18 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 readonly __dir __file
 
+if [ "${1:-}" == 'clean' ]; then
+  rm -rf ${__dir}/vendor/bundle
+fi
+
 if [ ! -d ${__dir}/vendor/bundle ]; then
-  bundle install --without plugins --verbose  --path vendor/bundle
+  # check bundler version
+  if [ '1.12.5' != "$(bundler --version | cut -d' ' -f 3)" ]; then
+    gem uninstall bundler \
+      --install-dir /usr/local/lib/ruby/gems/2.2.0
+    gem install --version 1.12.5 bundler \
+      --install-dir /usr/local/lib/ruby/gems/2.2.0
+  fi
+
+  bundle install --path vendor/bundle
 fi
