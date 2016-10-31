88826c2d25a4f84e8a6ec43d9ea64cceb2aebe75
Andrew Roslik <andkirby@gmail.com>
2016-10-31 22:56:09 +0200

    Updated docker installation approach.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
-FROM ruby:2.1-onbuild
-CMD ["sleep infinity"]
+FROM ruby:2.2.5-slim
+
+RUN apt-get -qq update
+RUN apt-get -qq -y install \
+    vim nano \
+    git curl
+
+RUN curl -Ls https://gist.github.com/andkirby/389f18642fc08d1b0711d17978445f2b/raw/bashrc_ps1_install.sh | bash && \
+    curl -Ls https://gist.github.com/andkirby/0e2982bee321aa611bc66385dee5f399/raw/bashrc_init_install.sh | bash
--- install-dev.sh
+++ install-dev.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-bundle install --without plugins --verbose  --path vendor/bundle
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+readonly __dir __file
+
+if [ ! -d ${__dir}/vendor/bundle ]; then
+  bundle install --without plugins --verbose  --path vendor/bundle
+fi
